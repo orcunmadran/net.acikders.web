3659b271e3bf5ab1b2514758652ffdffe3870f62
orcunmadran <orcunmadran@gmail.com>
2020-07-28 23:53:15 +0300

    Web Template
    First commit of acikders.net

---
+++ django/pages/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request, *args, **kwargs):
+    print(request.user) #User konsolda gözüküyor!
+    sozluk = { #Context sözlük olmak zorunda
+        "ad":"orçun",
+        "soyad":"madran"
+    }
+    return render(request, 'home.html', sozluk)
+
+def basic_view(*args, **kwargs):
+    return HttpResponse("<h1>TEST</h1><p><a href='javascript:history.back()'>Geri dön...</a></p>")
